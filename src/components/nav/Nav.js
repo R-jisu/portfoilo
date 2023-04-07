9d2a506763340079fc6e3f35c6b38c5d55c302f5
Ryu jisu <wltn1418@gmail.com>
2023-04-08 01:05:53 +0900

    nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,21 +1,31 @@
-import React from "react";
 import styles from "./Nav.module.scss";
 import { Link } from "react-router-dom";
 function Nav({ pathname }) {
-  console.log(pathname);
   return (
     <>
       <div className={styles.container}>
         <h2 className={styles.title}>Portfolio</h2>
         <ul className={styles.nav_ul}>
           <Link to="/">
-            <li>About Me</li>
+            <li className={pathname === "/" ? styles.Active : styles.notActive}>
+              About Me
+            </li>
           </Link>
           <Link to="/skills">
-            <li>Skills</li>
+            <li
+              className={
+                pathname === "/skills" ? styles.Active : styles.notActive
+              }
+            >
+              Skills
+            </li>
           </Link>
           <Link to="/works">
-            <li className={styles.submenu}>
+            <li
+              className={`${styles.submenu} ${
+                pathname === "/works" ? styles.Active : styles.notActive
+              }`}
+            >
               Works
               <ul className={styles.second_li}>
                 <li>project1</li>
@@ -24,8 +34,14 @@ function Nav({ pathname }) {
               </ul>
             </li>
           </Link>
-          <Link to="/Contact">
-            <li>Contact</li>
+          <Link to="/contact">
+            <li
+              className={
+                pathname === "/contact" ? styles.Active : styles.notActive
+              }
+            >
+              Contact
+            </li>
           </Link>
         </ul>
       </div>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,7 +1,67 @@
 import React from "react";
+import style from "./Skills.module.scss";
+import { CircularProgressBar } from "@tomickigrzegorz/react-circular-progress-bar";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <>
+      <div className={style.flexContainer}>
+        <div className={style.relative}>
+          <CircularProgressBar
+            colorCircle="#ededed"
+            colorSlice="#e54e21"
+            percent={80}
+            fontColor="#e54e21"
+            round={true}
+            fontSize="15px"
+            textPosition="1.5rem"
+          >
+            <div className={style.langHTML}>HTML</div>
+          </CircularProgressBar>
+        </div>
+        <div className={style.relative}>
+          <CircularProgressBar
+            colorCircle="#ededed"
+            colorSlice="#379ad6"
+            percent={80}
+            fontColor="#379ad6"
+            round={true}
+            fontSize="15px"
+            textPosition="1.5rem"
+          >
+            <div className={style.langCSS}>CSS</div>
+          </CircularProgressBar>
+        </div>
+
+        <div className={style.relative}>
+          <CircularProgressBar
+            colorCircle="#ededed"
+            colorSlice="#efd81d"
+            percent={55}
+            fontColor="#efd81d"
+            round={true}
+            fontSize="15px"
+            textPosition="1.5rem"
+          >
+            <div className={style.langJS}>JS</div>
+          </CircularProgressBar>
+        </div>
+        <div className={style.relative}>
+          <CircularProgressBar
+            colorCircle="#ededed"
+            colorSlice="#2cc1e8"
+            percent={10}
+            fontColor="#2cc1e8"
+            round={true}
+            fontSize="15px"
+            textPosition="1.5rem"
+          >
+            <div className={style.langReact}>React</div>
+          </CircularProgressBar>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Skills;
